31de4172b044a21bd5e115eb84d2e8c2226186cd
Ysmael Marcano <ysmaelmarcano21@gmail.com>
2021-09-29 14:41:30 -0400

    styling nav menu and adding funcionability

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 let hamburger= document.getElementById("hamburger-container");
+let menu = document.getElementById("header")
 hamburger.addEventListener("click", displaymenu)
+document.addEventListener("click", hidemenu)
+
 
 function displaymenu() {
-    let menu = document.getElementById("header")
     menu.classList.toggle("displayed")
+}
+function hidemenu(e) {
+    console.log(e.target.className)
+    if (e.target.className != "nav-link" && e.target
+    .className != "hamburger-icon") {
+        menu.classList.remove("displayed")
+    }
 }
