48b5d6b19a66f929afe65b1b826ff7cd3d43e726
Ysmael Marcano <ysmaelmarcano21@gmail.com>
2021-10-01 22:50:23 -0400

    adding prettier format

--- public/index.js
+++ public/index.js
@@ -1,47 +1,50 @@
-
 document.addEventListener("click", clickEventManager);
-document.addEventListener("scroll", ChangeDisplay)
-let hamburger= document.getElementById("hamburger-container");
+document.addEventListener("scroll", ChangeDisplay);
+let hamburger = document.getElementById("hamburger-container");
 let menu = document.getElementById("header");
 
 function clickEventManager(e) {
-
-if (e.target.className === "hamburger-container" || e.target.className === "hamburger-icon") {
-    menu.classList.toggle("displayed")
-}else if (e.target.className !=="nav-link"){
-    menu.classList.remove("displayed")
-}
+  if (
+    e.target.className === "hamburger-container" ||
+    e.target.className === "hamburger-icon"
+  ) {
+    menu.classList.toggle("displayed");
+  } else if (e.target.className !== "nav-link") {
+    menu.classList.remove("displayed");
+  }
 }
 
 /* Welcome Animation Containers */
 
 let welcome_container = document.getElementById("welcome-container");
-let welcome_section = document.getElementById("welcome-section")
+let welcome_section = document.getElementById("welcome-section");
 
-
-
-ChangeDisplay()
+ChangeDisplay();
 function ChangeDisplay() {
-    if(isInViewport(welcome_container)){
-        [...welcome_container.children].forEach((el)=>{el.classList.add("displayed")})
-    } else {
-        [...welcome_container.children].forEach((el)=>{el.classList.remove("displayed")})
-    }
+  if (isInViewport(welcome_container)) {
+    [...welcome_container.children].forEach((el) => {
+      el.classList.add("displayed");
+    });
+  } else {
+    [...welcome_container.children].forEach((el) => {
+      el.classList.remove("displayed");
+    });
+  }
 }
 
 function getActualPosition(element) {
-    const item = element.getBoundingClientRect().top
-    return item
+  const item = element.getBoundingClientRect().top;
+  return item;
 }
-
 
 /* IS IN VIEWPORT FUNCTION */
 function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
